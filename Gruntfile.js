cebf0598108770dd1d7efc800e4a73062b0be858
Hamzah <6hamzah6@gmail.com>
2016-04-23 08:46:52 -0700

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 
 'use strict'
 
-var ngrok = require('ngrok');
 
 module.exports = function (grunt) {
 
@@ -56,22 +55,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-newer');
-    // Register customer task for ngrok
-    grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function () {
-        var done = this.async();
-        var port = 8080;
 
-        ngrok.connect(port, function (err, url) {
-            if (err !== null) {
-                grunt.fail.fatal(err);
-                return done();
-            }
-            grunt.config.set('pagespeed.options.url', url);
-            grunt.task.run('pagespeed');
-            done();
-        });
-    });
-
-    // Register default tasks
-    grunt.registerTask('default', ['newer:concat', 'newer:imagemin', 'psi-ngrok']);
+    grunt.registerTask('default', ['newer:concat', 'newer:imagemin']);
 }
